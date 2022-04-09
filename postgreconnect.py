ebdde7efb840c1e52287d8357be8358aebbf39c2
MorsCrt <203301130@ogr.selcuk.edu.tr>
2022-04-09 13:49:17 -0700

    search and create query. Hint parameters

--- postgreconnect.py
+++ postgreconnect.py
@@ -3,8 +3,8 @@ import logging
 
 
 class PostgreDbOperations:
-    def __init__(self, database, user,
-                 password, host, port, autocommit):
+    def __init__(self, database:str, user:str,
+                 password:str, host:str, port:str, autocommit:bool):
         self.database = database
         self.user = user
         self.password = password
@@ -25,11 +25,21 @@ class PostgreDbOperations:
             self.connection.autocommit = self.autocommit
             self.cursor = self.connection.cursor()
 
-    def query(self, query):
+    def search_query(self, query:str,fetchall=False,fetchone=False):
         self.cursor.execute(query)
-        result = self.cursor.fetchall()
-        return result
-
+        if fetchall==True:
+            result = self.cursor.fetchall()
+            return result
+        elif fetchone==True:
+            result = self.cursor.fetchone()
+            return result
+        else:
+            print("Check fetch parameter!")
+            
+    def create_query(self,query:str):
+        self.cursor.execute(query)
+        return str("Done")
+    
     def close(self):
         self.cursor.close()
         self.connection.close()
